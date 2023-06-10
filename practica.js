74eb076b9fa9923167505e40c0b7a240ba10fae5
FerChoo9 <fernandoroberts185@gmail.com>
2023-06-09 23:13:13 -0300

    proyecto final coder

--- practica.js
+++ practica.js
@@ -17,16 +17,48 @@ function constructor(nombre, precio, clase){
     this.clase = clase
 }
 
+let analisisObjetos = [];
+
 
 const submitbutton = document.querySelector("#submitbutton")
 submitbutton.addEventListener("click", (e) => {
+    
 
     let nuevoObjeto = new constructor(nombre = document.querySelector("#nombre").value, precio = parseFloat(document.querySelector("#precio").value), clase = document.querySelector("#clase").value)
     console.log(nuevoObjeto)
     
     deudas.push(nuevoObjeto)
     console.log(deudas)
-    
+
+    let nuevoDiv = document.createElement("div")
+    nuevoDiv.setAttribute("style", `display: flex; width: auto; margin-bottom: 10px;`)
+
+    let nuevoUl = document.createElement("ul")
+    nuevoUl.setAttribute("style", `display: flex; list-style: none; margin-bottom: 10px;`)
+    nuevoUl.innerHTML=
+    `
+    <li style="margin-left: 10px;">${nuevoObjeto.nombre}</li>
+    <li style="margin-left: 10px;">${nuevoObjeto.precio}</li>
+    <li style="margin-left: 10px;">${nuevoObjeto.clase}</li>
+    `
+    let nuevoBoton = document.createElement("button")
+    nuevoBoton.textContent="Eliminar"
+    nuevoBoton.setAttribute("style", `display: flex; margin-left: 15px; border: 1px solid whitesmoke; border-radius: 3px; padding: 3px 3px 0 3px;`)
+    nuevoBoton.addEventListener("click", () => {
+
+        JSON.parse(localStorage.getItem("deudas"))
+
+        deudas.splice(deudas.indexOf(nuevoObjeto), 1)
+
+        localStorage.setItem("deudas", toJSON)
+
+        nuevoDiv.remove()
+    })
+
+    nuevoDiv.appendChild(nuevoUl)
+    nuevoDiv.appendChild(nuevoBoton)
+    document.querySelector(".reservadoObjetos").appendChild(nuevoDiv)
+
     const formulario = document.querySelector("#formulario")
     formulario.reset()
     
@@ -56,10 +88,10 @@ analisis.addEventListener("click", (e) => {
     let porcentaje = (sumaTotal*100)/monto
     console.log(porcentaje)
 
-
     
 
     const graficoCircular = document.querySelector("#ByNamee")
+
     const graficaBordeExt = document.createElementNS(svgNS, "circle")
     graficaBordeExt.innerHTML =
     graficaBordeExt.setAttribute("r", "121");
@@ -84,18 +116,19 @@ analisis.addEventListener("click", (e) => {
     grafica.setAttribute("r", "100")
     grafica.setAttribute("cx", "50%")
     grafica.setAttribute("cy", "50%")
-    grafica.setAttribute("style", `fill: none;stroke: darkcyan;stroke-width: 40px; stroke-dasharray: ${porcentaje}, 100; transform-origin: 50%; transform: rotate(-90deg);`)
+    grafica.setAttribute("style", `fill: none;stroke: darkcyan;stroke-width: 40px;stroke-dasharray: ${porcentaje}, 100; transform-origin: 50%; transform: rotate(-90deg);`)
     grafica.setAttribute("class", "animacionGrafica")
     graficoCircular.appendChild(grafica)
 
     grafica.setAttribute("pathLength", 100)
     graficoCircular.appendChild(grafica)
-    
+
+
     const descripcion1 = document.querySelector("#h3modificable")
     let h3decripcion = document.createElement("h3")
     h3decripcion.innerHTML=
     `
-    Estás consumiendo el ${porcentaje}% de tus ingresos totales
+    Estás consumiendo el ${Math.round(porcentaje)}% de tus ingresos.
     `
     descripcion1.appendChild(h3decripcion)
     
@@ -103,4 +136,64 @@ analisis.addEventListener("click", (e) => {
     //analisis por objeto
 
     
+    deudas.forEach(deudas => {
+        let objetoColores = {color: "#"+Math.ceil(Math.random()*1000000), precio: deudas.precio, porcentaje: Math.round(deudas.precio*100/(document.querySelector("#monto").value))}
+        console.log(objetoColores)
+        analisisObjetos.push(objetoColores)
+    })
+
+    analisisObjetos.forEach(analisisObjetos => {
+        let nuevoContenedor = document.createElement("div")
+        let nuevoAnalisis = document.createElement("ul")
+        nuevoAnalisis.setAttribute("style", `display: flex; list-style: none;`)
+        nuevoAnalisis.innerHTML=
+        `
+        <li><div style="width: 20px; height: 20px; background-color: ${analisisObjetos.color};margin-bottom: 10px; margin-right: 20px;"></div></li>
+        <li style="margin-bottom: 10px; margin-right: 20px;">${analisisObjetos.precio}</li>
+        <li style="margin-bottom: 10px; margin-right: 20px;">${analisisObjetos.porcentaje}%</li>
+        `
+        document.querySelector(".analisisEscrito").appendChild(nuevoContenedor)
+        nuevoContenedor.appendChild(nuevoAnalisis)
+    })
+
+
+
+    const graficoCircular2 = document.querySelector("#svgGraphic2")
+    const graficaBordeExt2 = document.createElementNS(svgNS, "circle")
+    graficaBordeExt2.innerHTML =
+    graficaBordeExt2.setAttribute("r", "121");
+    graficaBordeExt2.setAttribute("cx", "50%"); 
+    graficaBordeExt2.setAttribute("cy", "50%");
+    graficaBordeExt2.setAttribute("pathlength", "100");
+    graficaBordeExt2.setAttribute("style", `fill:none; stroke: whitesmoke; stroke-width: 2px; stroke-dasharray: 100, 0; transform-origin: 50%; transform: rotate(-90deg);`)
+    graficoCircular2.appendChild(graficaBordeExt2)
+
+    const graficaBordeInt2 = document.createElementNS(svgNS, "circle")
+    graficaBordeInt2.innerHTML =
+    graficaBordeInt2.setAttribute("r", "79");
+    graficaBordeInt2.setAttribute("cx", "50%");
+    graficaBordeInt2.setAttribute("cy", "50%");
+    graficaBordeInt2.setAttribute("pathlength", "100");
+    graficaBordeInt2.setAttribute("style", `fill:none; stroke: whitesmoke; stroke-width: 2px; stroke-dasharray: 100, 0; transform-origin: 50%; transform: rotate(-90deg);`)
+    graficoCircular2.appendChild(graficaBordeInt2)
+
+
+    let sumaPorcentajes = analisisObjetos.reduce((acc, analisisObjetos, index) => {
+        return acc+= analisisObjetos.porcentaje*index
+    }, 0)
+
+    analisisObjetos.forEach(analisisObjetos => {
+        let nuevoGrafico = document.createElementNS(svgNS, "circle")
+
+        nuevoGrafico.innerHTML=
+        nuevoGrafico.setAttribute("r", "100")
+        nuevoGrafico.setAttribute("cx", "50%")
+        nuevoGrafico.setAttribute("cy", "50%")
+        nuevoGrafico.setAttribute("style", `fill: none; stroke: ${analisisObjetos.color}; stroke-width: 40px; stroke-dasharray: ${((analisisObjetos.porcentaje)*360)/100}, 360; transform-origin: 50%; transform: rotate(${-90+ sumaPorcentajes }deg);`)
+
+        graficoCircular2.appendChild(nuevoGrafico)
+
+        nuevoGrafico.setAttribute("pathLength", 360)
+        graficoCircular2.appendChild(nuevoGrafico)
+    })
 })
